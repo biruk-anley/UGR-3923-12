e1da6d57dd38903cf2fb9e12cdd1d92b33a5c091
biruk <birukanley1991@gmail.com>
2021-12-18 18:04:39 +0300

    initial commit

---
+++ scripts/style.js
@@ -0,0 +1,7 @@
+const humburger = document.getElementById('humburger');
+const nav= document.getElementById('nav');
+
+humburger.addEventListener('click',()=>{
+    nav.classList.toggle('show');
+
+});
